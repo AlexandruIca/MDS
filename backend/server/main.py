3b2a1748185e5335eeae802e7a64705098b69c3d
Alexandru Ica <alexandruica703@gmail.com>
2021-02-26 18:04:09 +0200

    Setup sqlite3

--- backend/server/main.py
+++ backend/server/main.py
@@ -3,6 +3,8 @@ import tornado.options
 import tornado.ioloop
 import tornado.websocket
 
+import sqlite3
+
 from log import logger
 
 tornado.options.define('port', default=5634,
@@ -11,6 +13,11 @@ tornado.options.define('debug', default=True, help='Run in debug mode')
 
 config = tornado.options.options
 
+db = sqlite3.connect('server.db')
+db_cursor = db.cursor()
+
+db_cursor.execute('CREATE TABLE IF NOT EXISTS messages(content text)')
+
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin) -> bool:
@@ -18,10 +25,17 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
         logger.info('New connection!')
-        self.write_message('Hello World!')
+        to_send: str = ''
+
+        for row in db_cursor.execute('SELECT content FROM messages'):
+            to_send += f'{row[0]}\n'
+
+        self.write_message(to_send)
 
     def on_message(self, message):
         logger.info(f'Message received: {message}')
+        db_cursor.execute('INSERT INTO messages VALUES(?)', (message,))
+        db.commit()
 
     def on_close(self):
         logger.info('Connection closed!')
@@ -31,6 +45,7 @@ def main():
     app = tornado.web.Application([(r'/ws', WSHandler)])
     app.listen(config.port)
     tornado.ioloop.IOLoop.current().start()
+    db.close()
 
 
 if __name__ == '__main__':
