6e9507165285aa88da120708338513ae0670b27c
jungle335 <62486445+jungle335@users.noreply.github.com>
2021-05-19 08:46:46 +0300

    Update formular.js

--- client/formular.js
+++ client/formular.js
@@ -1,13 +1,72 @@
 var ws = new WebSocket("ws://localhost:5634/ws");
+const email = document.getElementById('email')
+const fst = document.getElementById('fst')
+const snd = document.getElementById('snd')
+const password = document.getElementById('pass')
 const create = document.getElementById('create')
+let verifEmail, verifPassword = 0
 
+email.addEventListener("input", () => {
+    const mailRegex = new RegExp('^([a-z]+[_0-9]*)+@[a-z]+(\.[a-z]+)+$')
+    const mailShow = document.getElementById('mailShow')
+    if(email.value)
+    {
+        if(mailRegex.test(email.value)) 
+        {
+            email.style.borderColor = "green"
+            verifEmail = 1
+            mailShow.innerHTML = 'GOOD'
+        }
+        else 
+        {
+            email.style.borderColor = "red"
+            verifEmail = 0
+            mailShow.innerHTML = 'YOUR EMAIL SHOUD NOT BE LIKE THAT'
+        }
+    }
+    else 
+        {
+            email.style.borderColor = "inherit"
+            mailShow.innerHTML = ''
+        }
+})
+
+
+// password.addEventListener("input", () => {
+//     const passRegex = new RegExp('^[A-Z]{1}[a-z0-9][\#\!\*]+$')
+//     const passShow = document.getElementById('passShow')
+//     if(password.value)
+//     {
+//         if(passRegex.test(password.value)) 
+//         {
+//             password.style.borderColor = "green"
+//             passShow.innerHTML = 'GOOD'  
+//             verifPassword = 1
+//         }
+//         else 
+//         {
+//             password.style.borderColor = "red"
+//             passShow.innerHTML = 'YOUR PASSWORD SHOULD CONTAIN AN UPPERCASE LETTER FOLLOWED BY SMALL LETTERS OR NUMBERS ENDING WITH ONE OF THESE SYMBOLS [#!*]'
+//             verifPassword = 0
+//         }
+//     }
+//     else 
+//         {
+//             password.style.borderColor = "inherit"
+//             passShow.innerHTML = ''
+//         }
+// })
 
 create.addEventListener("click", () => {
     let d = {
-        "Email": document.getElementById('email').value,
-        "Fst": document.getElementById('fst').value,
-        "Snd": document.getElementById('snd').value,
-        "Password": document.getElementById('pass').value 
+        "Email": email.value,
+        "Fst": fst.value,
+        "Snd": snd.value,
+        "Password": pass.value 
     }
-    ws.send(JSON.stringify(d));    
+    if(verifEmail){
+        ws.send(JSON.stringify(d));    
+    }else{
+        alert("Verifica datele")
+    }
 })
